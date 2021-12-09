c1a52a18c8ccdbdbcbb1f1193e41a16ea54cc0cf
amoham1 <amoham456@gmail.com>
2021-12-09 17:42:37 -0500

    refactor the interviewer list

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,7 +9,7 @@ export default function InterviewerListItem(props) {
     "interviewers__item--selected": props.selected,
   });
 
-  return (<li onClick={()=>{props.setInterviewer(props.id)}} className={interviewClass}>
+  return (<li onClick={props.setInterviewer} className={interviewClass}>
     <img
       className="interviewers__item-image"
       src={props.avatar}
